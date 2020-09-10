cb4bc128f07a71d2a9522b45108f58a4e3f56966
Opilane TTHK <opilane@tthk.ee>
2020-09-10 10:32:37 +0300

    Add project files.

---
+++ SliderStepper/SliderStepper/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SliderStepper
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            Editor1.FontSize = Slider1.Value;
+        }
+        private void Slider1_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (e.NewValue > 0)
+            {
+                Editor1.FontSize = e.NewValue;
+                Slider1.Value = e.NewValue;
+                Slider2.Value = e.NewValue;
+                Slider3.Value = e.NewValue;
+            }
+        }
+    }
+}
